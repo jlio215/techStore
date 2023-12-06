2e40263967c27543dcec306fd53bf71d6a00b5a5
X <x@MacBook-Air-de-X.local>
2023-12-05 20:21:34 -0500

    models, routes and controllers

---
+++ models/EstadoEquipo.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose')
+
+const EstadoEquipoSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'Nombre genero requerido'],
+        minlength: 1,
+        unique: true
+    },
+    estado: {
+        type: Boolean,
+        default: true,
+        required: true
+    },
+    fechaCreacion: {
+        type: Date,
+        default: new Date()
+    },
+    fechaActualizacion: {
+        type: Date,
+        default: new Date()
+    }
+})
+
+module.exports = model('EstadoEquipo', EstadoEquipoSchema)
